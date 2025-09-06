d8430ed67f494a08e47346264c5aa3c8d1541ae4
Ashish Pal <ashishpal@Ashishs-MacBook-Pro.local>
2025-09-06 19:25:03 +0530

    first commit

---
+++ src/utils.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+def save_object(file_path, obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for key, value in models.items():
+            model = value
+
+            para = params[key]
+
+            gs = GridSearchCV(model, param_grid=para, cv=3, n_jobs=-1, verbose=3, refit=True)
+            gs.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train) # train model
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[key]=test_model_score
+        
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
